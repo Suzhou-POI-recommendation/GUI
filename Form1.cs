b26f76e9032ba73c080a93aaa6e43d9b10b40f0f
张维昱 <weiyu_zhang@pku.edu.cn>
2020-05-16 16:25:51 +0800

    更新地图组件与定位功能

--- Form1.cs
+++ Form1.cs
@@ -2,7 +2,11 @@
  * Project: vector_to_raster
  * @file: Form1.cs
  * @说明：
- * 111
+ * @修改：
+ *  1.5.14-5.16：使用webbrowser构建地图功能
+ *  
+ * @TODO：
+ * 1. 如何通知客户无法获取坐标
  * *******************************************************************/
 using System;
 using System.Collections.Generic;
@@ -12,6 +16,8 @@ using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Windows.Forms;
+using System.Device.Location;
+using System.ComponentModel;
 
 namespace recommendation_system
 {
@@ -21,10 +27,70 @@ namespace recommendation_system
         public Form1()
         {
             InitializeComponent();
+            this.Load += new EventHandler(Form1_Load);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
 
+            //Uri url = new Uri("https://www.baidu.com/");
+            //忽略js脚本错误
+            map_webbrowser.ScriptErrorsSuppressed = true;
+            map_webbrowser.Navigate(Application.StartupPath+@"\map_page.html");
+            map_webbrowser.NewWindow += new CancelEventHandler(Web_NewWindow);
+            //map_webbrowser.Navigate(url.ToString());
+            GetLocationEvent(); 
         }
 
-        //变量
+        #region Web模块
+        //防止弹出IE窗口
+        private void Web_NewWindow(Object sender, CancelEventArgs e)
+        {
+            e.Cancel = true;
+            System.Text.StringBuilder messageBoxCS = new System.Text.StringBuilder();
+            messageBoxCS.AppendFormat("{0} = {1}", "Cancel", e.Cancel);
+            messageBoxCS.AppendLine();
+            MessageBox.Show(messageBoxCS.ToString(), "NewWindow Event");
+        }
+        #endregion
+
+
+        #region 定位模块
+        //定位器每次在调用定位函数时建立
+        GeoCoordinateWatcher watcher = null;
+        //html与winform交互
+        private void SetCenter(params object[] args)
+        {
+            //this.map_webbrowser.Document.InvokeScript("Test");
+            this.map_webbrowser.Document.InvokeScript("SetCenter",args);
+        }
+        //定位函数
+        public void GetLocationEvent()
+        {
+            watcher = new GeoCoordinateWatcher(GeoPositionAccuracy.High);
+            watcher.PositionChanged += new EventHandler<GeoPositionChangedEventArgs<GeoCoordinate>>(watcher_PositionChanged);
+            bool started = watcher.TryStart(false, TimeSpan.FromMilliseconds(2000));
+            if (!started)
+            {
+                MessageBox.Show("获取坐标超时");
+            }
+            //else if(watcher.Status!=GeoPositionStatus.Ready)
+            //{
+            //    MessageBox.Show("无法获取坐标");
+            //}
+            
+        }
+        //只有通过异步加载PositionChanged的方式才可以获得坐标
+        void watcher_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+        {
+            SetCenter(new object[] { Convert.ToDouble(e.Position.Location.Longitude), Convert.ToDouble(e.Position.Location.Latitude) });
+            MessageBox.Show($"您的位置：\nLatitude: {e.Position.Location.Latitude}, " +
+                $"Longitude {e.Position.Location.Longitude}");
+            watcher.PositionChanged -= new EventHandler<GeoPositionChangedEventArgs<GeoCoordinate>>(watcher_PositionChanged);
+            watcher.Dispose();
+            //保证只定位一次
+
+        }
+        #endregion
 
 
         //鼠标拖动整体窗口函数（由于去除了边框）
